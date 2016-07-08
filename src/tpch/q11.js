04ec81acab6db8b78a9ba9033351c161018fd6b3
jackbauer35 <kimgebaly@yahoo.com>
2016-07-07 21:26:12 -0400

    tpch query 11

---
+++ src/tpch/q11.js
@@ -0,0 +1,24 @@
+
+function query11(){
+	  sup_nat=ABi.select()
+		.from("supplier").join("nation").on("s_nationkey","n_nationkey")
+		.field("s_suppkey")
+		.where(eq("n_name",'GERMANY'))
+		.materialize();
+
+	  ps_sup=ABi.select()
+		.from("partsupp").join(sup_nat).on("ps_suppkey","s_suppkey")
+		.field("ps_partkey",as(sum(mul("ps_supplycost","ps_availqty")),"value"))
+		.group("ps_partkey")
+		.materialize();
+
+	  thresh=ABi.select()
+		.from(ps_sup)
+		.field(sum("value")).eval();
+
+	  return=ABi.select()
+		.from(ps_sup)
+		.field("ps_partkey","value")
+		.where(gt("value",thresh*0.0001))
+		.order([-1]);
+}
