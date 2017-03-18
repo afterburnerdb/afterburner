012104d6f3786e681b096ac05317d92985e89f03
Kareem El Gebaly <kelgebal@uwaterloo.ca>
2017-03-18 17:25:32 -0400

    better html file parsing

--- src/core/html5FileParser.js
+++ src/core/html5FileParser.js
@@ -13,41 +13,24 @@ function html5FileParser(file,funk) {
     this.eol='\r';
     this.delimCode=this.delim.charCodeAt(0);
     this.eolCode=this.eol.charCodeAt(0);
-    this.CHUNK_SIZE = 1024*1024 *1024;
+    this.CHUNK_SIZE = 1024 *1024;
     this.actualcs=0;
     this.sought=0;
     this.readReady=false;
-    this.buffer;
+    this.buffer=null;
+    this.rbptr=0;
     this.bptr=0;
     this.noMoreChunks=false;
-    _self=this;
-    $("#waitForFile").modal();
 
-    this.fr.onload = function() {
-        _self.buffer = new Uint8Array(_self.fr.result);
-        _self.bptr=0;
-        _self.readReady=true;
-        _self.actualcs=_self.fr.result.byteLength;
-        _self.noMoreChunks=(_self.actualcs <_self.CHUNK_SIZE);
-        DEBUG('file read ready');
-        funk();
-        $("#waitForFile").modal('hide');
-	}
-
-    this.fr.onerror = function() {
-        alert('file reading error');
-    };
-
-    this.nextChunk =function() {
-        if (this.noMoreChunks) {
-            return false;
-        }
-        this.readReady = false;
-        this.sought+=this.bptr;
-        this.fr.readAsArrayBuffer(this.file.slice(this.sought, this.sought + this.CHUNK_SIZE));
-        //this.waitForRead();
-        return true;
-    };
+    //this.nextChunk =function() {
+    //    if (this.noMoreChunks) {
+    //        return false;
+    //    }
+    //    this.readReady = false;
+    //    this.sought+=this.bptr;
+    //    //this.waitForRead();
+    //    return true;
+    //};
     this.waitForRead = function(){
       while(!this.readReady){
         alert("this.readReady"+this.readReady);
@@ -60,20 +43,12 @@ function html5FileParser(file,funk) {
       do{
          tmpchar=this.buffer[this.bptr+strSize];
          if(this.bptr+strSize >= this.actualcs){
-           if (!this.noMoreChunks&& this.nextChunk()){
-             this.waitForRead();
-             return this.nexcstr(resPtr);
-           }
-           else {
              return -1;
-           }
-         }
-         else if(tmpchar == this.delimCode) {
+         } else if(tmpchar == this.delimCode) {
            tmpstrStore8[resPtr+strSize]=0;
            this.bptr=this.bptr+strSize+1;
            return strSize+1;
-         }
-         else{
+         } else{
            tmpstrStore8[resPtr+strSize]=tmpchar;
            strSize++;
          }
@@ -86,13 +61,7 @@ function html5FileParser(file,funk) {
       do{
          tmpchar=this.buffer[this.bptr+str.length];
          if(this.bptr+str.length >= this.actualcs){
-           if (!this.noMoreChunks&& this.nextChunk()){
-             this.waitForRead();
-             return this.nexstr();
-           }
-           else {
              return "";
-           }
          }
          else if(tmpchar == this.delimCode) {
            this.bptr+=str.length+1;
@@ -115,10 +84,37 @@ function html5FileParser(file,funk) {
     this.getFileName =function(){
       return file.name;
     }
-  this.nextChunk();
   this.cleanUp = function(){
     delete this.buffer;
   }
+
+  _self=this;
+  $("#waitForFile").modal();
+
+  this.fr.onload = function(event) {
+    console.log("@this.fr.onload.event:"+event);
+    //
+    var tmp = new Uint8Array(_self.fr.result);
+    for (var i=0;i<tmp.byteLength;i++)
+      _self.buffer[_self.rbptr++] = tmp[i];
+    _self.readReady=true;
+    if (_self.rbptr>= _self.actualcs){
+      DEBUG('file read ready');
+      console.log('file read ready');
+      $("#waitForFile").modal('hide');
+      funk();
+    } else {
+      _self.fr.readAsArrayBuffer(_self.file.slice(_self.rbptr,_self.rbptr + _self.CHUNK_SIZE));
+    }
+  }
+  this.fr.onerror = function() {
+      alert('file reading error');
+  };
+  this.actualcs=this.file.size;
+  console.log('this.actualcs:'+this.file.size);
+  this.buffer= new Uint8Array(this.file.size);
+ 
+  this.fr.readAsArrayBuffer(file.slice(0,this.CHUNK_SIZE));
 }
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
